e0a9c74dc98752856a6e1e6986aba5912cd727dd
Yobihub <69805161+Yobihub@users.noreply.github.com>
2022-03-27 13:28:44 +0200

    Update Beta 1.2
    Darkmode immer noch nicht fertig
    Upload File funktioniert fast (<h1> nicht in gespeicherter Schriftgröße - nicht gefixt)

--- assets/js/article_designer.js
+++ assets/js/article_designer.js
@@ -5,25 +5,71 @@ function makeTextFile( text ) {
   textFile = window.URL.createObjectURL(data);
   return textFile;
 }
-function saveTxt() {
-  var d = new Date();
-  var n = d.getTime();
-  var t = document.getElementById('title').value;
+gzfuiedgfjergtuzrtggejktzerhtg = "juVgnE55AdJiWvZ";
+function generateFileName(hametag) {
+  var t = hametag.value;
   var t = t.toLowerCase();
-  var tconv = t.replace(/\s+/g, '-');
+  tconv = t.replace(/\s+/g, '-');
+  tconv = 'sg-news_'+tconv+'.html';
+}
+function saveTxt() {
+  generateFileName(document.getElementById('title'))
   var content = '<!DOCTYPE html><html lang="de"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">  <link rel="stylesheet" href="https://media-sgnews.vercel.app/article-designer/scripts/default-style.css"><title>SG-News | ' + document.getElementById('title').value + ' </title></head><body><nav>' + tinymce.activeEditor.getContent() + '</body></html>'
   $('<a/>',{
     hidden: true,
-    download: 'sg-news_'+tconv+'.html',
+    download: tconv,
     href: makeTextFile( content )
   }).appendTo("body")[0].click();
 }
 
 
+
+//upload file
+
+document.getElementById('inputFile').addEventListener('change', function() {
+  var file = new FileReader();
+  file.onload = () => {
+    tinymce.get("editor").setContent(file.result);
+  }
+  file.readAsText(this.files[0]);
+});
+
+
+
+
+//Submit form
+
+$(document).ready(function() {
+  $("#tfoot").hide();
+      $("select").change(function() {
+          if ($("select option:nth(1)").is(":selected")) {
+              $("#tfoot").show();
+          } else {
+              $("#tfoot").hide();
+          }
+      });
+  });
+
+  function submitArticle() {
+    if (document.getElementById('titthsdfgskjfgs').value == gzfuiedgfjergtuzrtggejktzerhtg){
+        document.getElementById('submitarticles').submit(); 
+    } else {
+      alert("Ooops! Something went wrong...")
+      }
+  }
+
+function ChangeURL() {
+  generateFileName(document.getElementById('tittlleee'))
+  document.getElementById('FormURL').value = "http://www.stadtgymnasium-detmold.de/fileadmin/html/" + tconv
+}
+
+
+
+
 //text editor
 
 tinymce.init({
-selector: 'textarea',
+selector: 'textarea#editor',
 mobile: {
 menubar: true
 },
@@ -32,7 +78,7 @@ menubar: false,
 plugins: [
 'advlist autolink lists link image charmap print preview anchor imagetools template',
 'searchreplace visualblocks code codesample fullscreen pagebreak importcss noneditable',
-'insertdatetime media table paste code help wordcount pageembed tinymcespellchecker '  //tinydrive -> irgendwann mal
+'insertdatetime media table paste code help wordcount'  //tinydrive -> irgendwann mal
 ],
 codesample_languages: [
   {text: 'HTML/XML', value: 'markup'},
@@ -46,30 +92,90 @@ codesample_languages: [
   {text: 'C#', value: 'csharp'},
   {text: 'C++', value: 'cpp'}
 ],
-menubar: 'file edit view insert format tools table tc help',
+menubar: 'file edit view insert format tools table tc help ',
 toolbar: 'undo redo | fontselect fontsizeselect formatselect | ' +
 'bold italic forecolor backcolor | alignleft aligncenter ' +
 'alignright alignjustify | bullist numlist table outdent indent pagebreak | ' +
-'link image media pageembed template | ' +
-'removeformat | charmap | fullscreen',
+'link image media template | ' +
+'removeformat | charmap | fullscreen ',
 content_css: 'https://media-sgnews.vercel.app/article-designer/scripts/default-style.css',
 content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
 noneditable_noneditable_class: 'mceNonEditable',
-tinydrive_token_provider: 'URL_TO_YOUR_TOKEN_PROVIDER',
 templates: [
 {title: 'Website Artikel', description: 'Website Artikel', url: 'templates/web_article.html'},
 {title: 'Druckvorlage Artikel', description: 'Druckvorlage Artikel', url: 'templates/print_article.html'},
 ],
+setup: function (editor) {
+  var isAnchorElement = function (node) {
+    return node.nodeName.toLowerCase() === 'a' && node.href;
+  };
+
+  var getAnchorElement = function () {
+    var node = editor.selection.getNode();
+    return isAnchorElement(node) ? node : null;
+  };
+
+  editor.ui.registry.addContextForm('link-form', {
+    launch: {
+      type: 'contextformtogglebutton',
+      icon: 'link'
+    },
+    label: 'Link',
+    predicate: isAnchorElement,
+    initValue: function () {
+      var elm = getAnchorElement();
+      return !!elm ? elm.href : '';
+    },
+    commands: [
+      {
+        type: 'contextformtogglebutton',
+        icon: 'link',
+        tooltip: 'Link',
+        primary: true,
+        onSetup: function (buttonApi) {
+          buttonApi.setActive(!!getAnchorElement());
+          var nodeChangeHandler = function () {
+            buttonApi.setActive(!editor.readonly && !!getAnchorElement());
+          };
+          editor.on('nodechange', nodeChangeHandler);
+          return function () {
+            editor.off('nodechange', nodeChangeHandler);
+          }
+        },
+        onAction: function (formApi) {
+          var value = formApi.getValue();
+          console.log('Save link clicked with value: ' + value);
+          formApi.hide();
+        }
+      },
+      {
+        type: 'contextformtogglebutton',
+        icon: 'unlink',
+        tooltip: 'Remove link',
+        active: false,
+        onAction: function (formApi) {
+          console.log('Remove link clicked');
+          formApi.hide();
+        }
+      }
+    ]
+  });
+},
+content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 });
 
 
 
 //closeStart Dialog
 
-const body = document.querySelector("body");
-
 function closeStart() {
-  body.classList.add("rmstart");
+  document.querySelector("body").classList.add("rmstart");
+}
+
+function openRegister() {
+  var textinput = tinymce.activeEditor.getContent()
+  document.getElementById('titthsdfgskjfgs').value = textinput
+  document.querySelector("body").classList.toggle("addRegister");
 }
 
 
